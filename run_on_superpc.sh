2e175d214d1c2f4c0e95970ee49f52f7465dcf6e
samuel-cavalcanti <scavalcanti111@gmail.com>
2023-04-27 19:04:03 -0300

    não é necessário do container para rodar com cuda

--- run_on_superpc.sh
+++ run_on_superpc.sh
@@ -2,14 +2,7 @@
 #SBATCH --job-name=neural_train
 #SBATCH --time=0-0:15
 #SBATCH --partition=gpu
-
-
-echo "loading nvidia container"
-module load singularity
-singularity shell /opt/npad/shared/containers/nvhpc_22.5_devel.sif
-
-echo "loading your .bashrc"
-source ~/.bashrc
+#SBATCH --exclusive
 
 # informando ao tch-rs que desejo compilar com cuda na versão 11.7
 export TORCH_CUDA_VERSION=cu117
